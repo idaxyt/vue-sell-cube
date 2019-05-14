3a6f7343a74320e04e4e1f2ed5d6e91ccec81ed8
idaxyt <waitingida@live.com>
2019-05-14 11:29:22 +0800

    引入stylus

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import './cube-ui'
 import App from './App.vue'
 
+import './common/stylus/index.styl'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')

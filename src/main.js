5ea6e53e3d28a1595404a21f926e3360584f65a6
idaxyt <waitingida@live.com>
2019-05-15 22:01:32 +0800

    vue-resource插件引入

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import './cube-ui'
 import App from './App.vue'
 import router from './router'
+import resource from 'vue-resource'
 
 import './common/stylus/index.styl'
 Vue.config.productionTip = false
+Vue.use(resource)
 
 new Vue({
     render: h => h(App),

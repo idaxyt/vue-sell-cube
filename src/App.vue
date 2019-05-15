e28bec73f418c5b40d48e292bc80e2433ad8f223
idaxyt <waitingida@live.com>
2019-05-15 19:48:40 +0800

    路由跳转页面链接

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import VHeader from './components/v-header/v-header'
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    VHeader
   }
 };
 </script>
 
-<style>
-#app {
+<style lang='stylus'>
+#app 
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+  .tab
+    display: flex
+    width:  100%
+    height: 40px
+    line-height: 40px
+    .tab-item
+      text-align: center
+      flex: 1
 </style>

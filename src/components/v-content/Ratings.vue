617f639fc41da9429359d9f06a58fbada80e4dd6
idaxyt <waitingida@live.com>
2019-05-21 14:59:27 +0800

    评论overview基础渲染+手机兼容性调试

--- src/components/v-content/Ratings.vue
+++ src/components/v-content/Ratings.vue
@@ -1,15 +1,111 @@
+import BScroll from 'better-scroll';
 <template>
-    <div>
-        ratings
+    <div class="ratings">
+        <div class="ratings-content">
+            <div class="overview">
+                <div class="overview-left">
+                    <h1 class="score">{{seller.score}}</h1>
+                    <div class="title">综合评分</div>
+                    <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+                </div>
+                <div class="overview-right">
+                    <div class="score-wrapper">
+                        <span class="title">服务态度</span>
+                        <Star :size='36' :score='seller.serviceScore'></Star>
+                        <span class="score">{{seller.serviceScore}}</span>
+                    </div>
+                    <div class="score-wrapper">
+                        <span class="title">商品评分</span>
+                        <Star class="star" :size='36' :score='seller.foodScore'></Star><span class="score">{{seller.foodScore}}</span>
+                    </div>
+                    <div class="delivery-wrapper">
+                        <span class="title">送达时间</span>
+                        <span class="delivery">{{seller.deliveryTime}}分钟</span>
+                    </div>
+                </div> 
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import Star from '../star/star'
 export default {
-    name: 'Ratings'
+    name: 'Ratings',
+    props: {
+        seller: {
+            type: Array | Object,
+            default() {
+                return {}
+            }
+        }
+    },
+    components: {
+        Star
+    }
 }
 </script>
 
-<style lang="stylus">
-    
+<style lang="stylus" scoped>
+    .ratings
+        position: absolute 
+        top: 174px
+        left: 0
+        width: 100%
+        overflow: hidden
+        .overview
+            display: flex
+            padding: 18px 0
+            .overview-left
+                flex: 0 0 137px
+                padding: 6px 0
+                width: 137px
+                text-align: center
+                border-right: 1px solid rgba(7,17,27,0.1)
+                .score
+                    margin-bottom: 6px
+                    height: 28px
+                    line-height: 28px
+                    font-size: 24px
+                    color: rgb(255,153,0)
+                .title
+                    margin-bottom: 8px
+                    line-height: 12px
+                    font-size: 12px
+                    color: rgb(7,17,27)
+                .rank
+                    line-height: 10px
+                    font-size: 10px
+                    color: rgb(147,153,159)
+            .overview-right
+                flex: 1
+                padding-left: 24px
+                .score-wrapper, .delivery-wrapper
+                    margin-bottom: 8px
+                    margin-top: 5px
+                    font-size: 0
+                    .title
+                        display: inline-block
+                        vertical-align: top
+                        line-height: 18px
+                        font-size: 12px
+                        color: rgb(7,17,27)
+                    .star
+                        display: -webkit-inline-flex;
+                        vertical-align: top
+                        line-height: 18px
+                        margin: 0 12px
+                        width: 100px
+                    .score
+                        display: inline-block
+                        vertical-align: top
+                        line-height: 18px
+                        font-size: 12px
+                        color: rgb(255,153,0)
+                    .delivery
+                        margin: 12px
+                        font-size: 12px
+                        line-height: 18px
+                        color: rgb(147,153,159)
+                    
 </style>

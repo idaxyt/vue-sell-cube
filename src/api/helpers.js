c74c5f7a2dd5094d535e3da052d02edd4af57b38
idaxyt <waitingida@live.com>
2019-05-22 23:24:25 +0800

    axio包装函数请求数据

---
+++ src/api/helpers.js
@@ -0,0 +1,17 @@
+import axois from 'axios'
+
+const ERR_OK = 0
+export function get(url) {
+    return function(params) {
+        return axois.get(url, {
+            params
+        }).then((res) => {
+            const { errno, data } = res.data
+            if (errno === ERR_OK) {
+                return data
+            }
+        }).catch(() => {
+
+        })
+    }
+}

8994264b07be88158ec4004e550e304c20e09449
idaxyt <waitingida@live.com>
2019-05-27 15:47:03 +0800

    在切换tab时调用商品等组件获取数据的方法

--- src/components/tab/Tab.vue
+++ src/components/tab/Tab.vue
@@ -18,8 +18,8 @@
                 :options='slideOptions'
                 @change='onChange'
                 @scroll="onScroll">
-                <cube-slide-item v-for="(tab,index) in tabs" :key='index'>
-                    <component :is="tab.component" :data='tab.data'></component>
+                <cube-slide-item v-for="(tab,tabIndex) in tabs" :key='tabIndex'>
+                    <component :is="tab.component" :data='tab.data' ref='component'></component>
                 </cube-slide-item>
             </cube-slide>
         </div>
@@ -68,6 +68,8 @@ export default {
     methods: {
         onChange(current) {
             this.index = current
+            const component = this.$refs.component[current]
+            component.fetch && component.fetch()
         },
         onScroll(pos) {
             const tabBarWidth = this.$refs.tabBar.$el.clientWidth

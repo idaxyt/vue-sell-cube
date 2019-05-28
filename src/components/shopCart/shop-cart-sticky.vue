e6bdf5eda3a786ea0ad0b07320bbd1f8102b417e
idaxyt <waitingida@live.com>
2019-05-28 15:37:50 +0800

    初始化shop-cart-sticky组件,调用shopCart组件

---
+++ src/components/shopCart/shop-cart-sticky.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="shop-cart-sticky" v-show='visible'>
+        <shop-cart
+            ref='shopCart'
+            :selectFoods='selectFoods'
+            :seller='seller'
+        >
+
+        </shop-cart>
+    </div>
+</template>
+
+<script>
+import ShopCart from './ShopCart'
+export default {
+    name: 'shop-cart-sticky',
+    data() {
+        return {
+            visible: false
+        }
+    },
+    props: {
+        selectFoods: {
+            type: Array,
+            default() {
+                return []
+            }
+        },
+        seller: {
+            type: Object,
+            default() {
+                return {}
+            }
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    
+</style>

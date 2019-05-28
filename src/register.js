63370b4bcf252b2427fd7c8c86ba961bf358bf90
idaxyt <waitingida@live.com>
2019-05-28 15:43:57 +0800

    注册ShopCartSticky组件为api

--- src/register.js
+++ src/register.js
@@ -2,6 +2,8 @@ import { createAPI } from 'cube-ui'
 import Vue from 'vue'
 import HeaderDetail from './components/v-header/header-detail'
 import ShopCartList from './components/shopCart/shop-cart-list'
+import ShopCartSticky from './components/shopCart/shop-cart-sticky'
 
 createAPI(Vue, HeaderDetail)
 createAPI(Vue, ShopCartList)
+createAPI(Vue, ShopCartSticky)

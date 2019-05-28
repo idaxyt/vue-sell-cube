db1706d729e58e8ddbd2cf16ae3ba98851e902ef
idaxyt <waitingida@live.com>
2019-05-28 14:56:16 +0800

    把shop-cart-list组件变为api

--- src/register.js
+++ src/register.js
@@ -1,5 +1,7 @@
 import { createAPI } from 'cube-ui'
 import Vue from 'vue'
 import HeaderDetail from './components/v-header/header-detail'
+import ShopCartList from './components/shopCart/shop-cart-list'
 
 createAPI(Vue, HeaderDetail)
+createAPI(Vue, ShopCartList)

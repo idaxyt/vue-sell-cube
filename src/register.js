e7c8c816b3578d5d78b4ee341b80d60612e511d2
idaxyt <waitingida@live.com>
2019-05-29 16:45:47 +0800

    注册商品详情页面

--- src/register.js
+++ src/register.js
@@ -3,7 +3,9 @@ import Vue from 'vue'
 import HeaderDetail from './components/v-header/header-detail'
 import ShopCartList from './components/shopCart/shop-cart-list'
 import ShopCartSticky from './components/shopCart/shop-cart-sticky'
+import Food from './components/food/food'
 
 createAPI(Vue, HeaderDetail)
 createAPI(Vue, ShopCartList)
 createAPI(Vue, ShopCartSticky)
+createAPI(Vue, Food)

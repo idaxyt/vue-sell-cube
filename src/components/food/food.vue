5a0e1bea814ddc1f29d64e5e684eb0a38f2019df
idaxyt <waitingida@live.com>
2019-05-29 16:49:49 +0800

    添加监听show变化的滚动事件

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -3,28 +3,28 @@
         <div class="food" v-show='visible'>
             <cube-scroll ref='scroll'>
                 <div class="food-content">
-                    <div class="img-header">
+                    <div class="image-header">
                         <img :src="food.image" alt="">
                         <div class="back" @click='hide'>
                             <i class="icon-arrow_lift"></i>
                         </div>
                     </div>
-                </div>
-                <div class="content">
-                    <div class="title">{{food.name}}</div>
-                    <div class="detail">
-                        <span class="sell-count">月售{{food.sellCount}}份</span>
-                        <span class="rating">好评率{{food.rating}}</span>
+                    <div class="content">
+                        <div class="title">{{food.name}}</div>
+                        <div class="detail">
+                            <span class="sell-count">月售{{food.sellCount}}份</span>
+                            <span class="rating">好评率{{food.rating}}</span>
+                        </div>
+                        <div class="price">
+                            <span class="now">￥{{food.price}}</span><span class="old" v-show='food.oldPrice'>￥{{food.oldPrice}}</span>
+                        </div>
+                        <split v-show='food.info'></split>
+                        <div class="info" v-show='food.info'>
+                            <h1 class="title">{{food.name}}</h1>
+                            <p class="text">{{food.info}}</p>
+                        </div>
+                        <Split></Split>
                     </div>
-                    <div class="price">
-                        <span class="noe">￥{{food.price}}</span><span class="old" v-show='food.oldPrice'>￥{{food.oldPrice}}</span>
-                    </div>
-                    <split v-show='food.info'></split>
-                    <div class="info" v-show='food.info'>
-                        <h1 class="title">{{food.name}}</h1>
-                        <p class="text">{{food.info}}</p>
-                    </div>
-                    <Split></Split>
                 </div>
             </cube-scroll>
         </div>
@@ -87,6 +87,7 @@
 </template>
 
 <script>
+    const EVENT_SHOW = 'show'
     const POSITIVE = 0;
     const NEGATIVE = 1;
     const ALL = 2
@@ -136,6 +137,13 @@ export default {
             return this.value
         }
     },
+    created() {
+        this.$on(EVENT_SHOW, ()=> {
+            this.$nextTick(()=>{
+                this.$refs.scroll.refresh()
+            })
+        })
+    },
     methods: {
         back() {
             this.$emit('input',false)
@@ -175,154 +183,149 @@ export default {
 
 <style lang='stylus' scoped> 
 @import "../../common/stylus/mixin"
-    .food 
-        position: fixed
-        left: 0
+@import "~common/stylus/variable"
+.food
+    position: fixed
+    left: 0
+    top: 0
+    bottom: 48px
+    z-index: 30
+    width: 100%
+    background: $color-white
+    &.move-enter-active, &.move-leave-active
+      transition: all 0.3s linear
+    &.move-enter, &.move-leave-active
+      transform: translate3d(100%, 0, 0)
+    .image-header
+      position: relative
+      width: 100%
+      height: 0
+      padding-top: 100%
+      img
+        position: absolute
         top: 0
+        left: 0
         width: 100%
         height: 100%
-        bottom: 48px
-        z-index: 30
-        background: #fff
-        &.move-enter-active, &.move-leave-active
-            transition: all 0.2s linear
-            transform: translate3d(0,0,0)
-        &.move-enter, &.move-leave-to
-            transform: translate3d(100%,0,0)
-        .image-header
-            position: relative
-            width: 100%
-            // height为0，padding-top为100% 能打造一个等比效果
-            height: 0
-            padding-top: 100%
-            img 
-                position: absolute
-                top: 0
-                left: 0
-                width: 100%
-                height: 100%
-            .back
-                position: absolute 
-                top: 10px
-                left: 0
-                .icon-arrow_lift
-                    display: block
-                    padding: 10px
-                    font-size: 20px
-                    color: #fff
-        .content
-            padding: 18px  
-            position: relative
-            .title
-                font-size: 14px
-                line-height: 14px
-                margin-bottom: 8px
-                font-weight: 700
-                color: rgb(7,17,27)    
-            .detail
-                margin-bottom: 18px
-                font-size: 0
-                line-height: 10px
-                .sell-count, .rating
-                    font-size: 10px
-                    color: rgb(147,153,159)
-                .sell-count
-                        margin-right: 12px
-            .price
-                    font-weight: 700
-                    line-height: 24px
-                    .now
-                        margin-right: 8px
-                        font-size: 14px
-                        color: rgb(240,24,20)
-                    .old
-                        font-size: 10px
-                        color: rgb(147,153,1559)
-                        text-decoration: line-through
-            .cartcontrol-wrapper
-                position: absolute 
-                right: 12px
-                bottom: 18px
-            .buy
-                position: absolute
-                right: 18px
-                bottom: 18px
-                z-index: 10
-                height: 24px
-                line-height: 24px
-                padding: 0 12px
-                box-sizing: border-box
-                font-size: 10px
-                border-radius: 12px
-                color: #ffffff
-                background: rgb(0,160,220)
-                &.fade-transition
-                    transition: all 0.2s
-                    opacity: 1
-                &.fade-enter, .fade-leave
-                    opacity: 0
-        .info
-            padding: 18px
-            .title
-                line-height: 14px
-                margin-bottom: 6px
-                font-size: 14px
-                color: rgb(7,17,27)
-            .text
-                color: rgb(77,85,93)
-                line-height: 24px
-                padding: 0 8px
-                font-size: 12px
-        .rating
-            padding-top: 18px
-            .title
-                font-size: 14px
-                line-height: 14px
-                margin-left: 18px
-                font-weight: 700
-                color: rgb(7,17,27)
-        .rating-wrapper
-            padding: 0 18px
-            padding-bottom: 48px
-            .rating-item
-                position: relative 
-                padding: 16px 0 
-                border-1px(rgba(7,17,27,0.1))
-                .user
-                    position: absolute 
-                    right: 0
-                    top: 16px
-                    font-size: 0
-                    line-height: 12px
-                    .name
-                        display: inline-block
-                        margin-right: 6px
-                        vertical-align: top
-                        font-size: 10px
-                        color: rgb(147,153,159)
-                    .avater
-                        border-radius: 50%
-                .time
-                    margin-bottom: 6px
-                    line-height: 12px
-                    font-size: 10px
-                    color: rgb(147,153,159)
-                .text
-                    line-height: 16px
-                    font-size: 12px
-                    color: rgb(7,17,27)
-                .icon-thumb_up, .icon-thumb_down
-                    line-height: 16px
-                    margin-right: 4px
-                    font-size: 12px
-                .icon-thumb_up
-                    color: rgb(0,160,220)
-                .icon-thumb_down
-                    color: rgb(147,153,159)
-            .no-rating
-                padding: 16px 0
-                font-size: 12px
-                color: rgb(147,153,159)
+      .back
+        position: absolute
+        top: 10px
+        left: 0
+        .icon-arrow_lift
+          display: block
+          padding: 10px
+          font-size: $fontsize-large-xx
+          color: $color-white
+    .content
+      position: relative
+      .title
+        line-height: 14px
+        margin-bottom: 8px
+        padding: 18px 0 0 18px
+        font-size: $fontsize-medium
+        font-weight: 700
+        color: $color-dark-grey
+      .detail
+        margin-bottom: 18px
+        padding: 0 18px
+        line-height: 10px
+        height: 10px
+        .sell-count, .rating
+          font-size: $fontsize-small-s
+          color: $color-light-grey
+        .sell-count
+          margin-right: 12px
+      .price
+        line-height: 24px
+        font-weight: 700
+        padding:0 0 18px 18px
+        .now
+          margin-right: 8px
+          font-size: 14px
+          color: $color-red
+        .old
+          text-decoration: line-through
+          font-size: $fontsize-small-s
+          color: $color-light-grey
+      .cart-control-wrapper
+        position: absolute
+        right: 12px
+        bottom: 12px
+      .buy
+        position: absolute
+        right: 18px
+        bottom: 18px
+        z-index: 10
+        height: 24px
+        line-height: 24px
+        padding: 0 12px
+        box-sizing: border-box
+        border-radius: 12px
+        font-size: $fontsize-small-s
+        color: $color-white
+        background: $color-blue
+        opacity: 1
+        &.fade-enter-active, &.fade-leave-active
+          transition: all 0.3s
+        &.fade-enter, &.fade-leave-active
+          opacity: 0
+          z-index: -1
+    .info
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 6px
+        font-size: $fontsize-medium
+        color: $color-dark-grey
+      .text
+        line-height: 24px
+        padding: 0 8px
+        font-size: $fontsize-small
+        color: $color-grey
+    .rating
+      padding-top: 18px
+      .title
+        line-height: 14px
+        margin-left: 18px
+        font-size: $fontsize-medium
+        color: $color-dark-grey
+      .rating-wrapper
+        padding: 0 18px
+        .rating-item
+          position: relative
+          padding: 16px 0
+          &:last-child
+            border-none()
+          .user
+            position: absolute
+            right: 0
+            top: 16px
+            display: flex
+            align-items: center
+            line-height: 12px
+            .name
+              margin-right: 6px
+              font-size: $fontsize-small-s
+              color: $color-light-grey
+            .avatar
+              border-radius: 50%
+          .time
+            margin-bottom: 6px
+            line-height: 12px
+            font-size: $fontsize-small-s
+            color: $color-light-grey
+          .text
+            line-height: 16px
+            font-size: $fontsize-small
+            color: $color-dark-grey
+            .icon-thumb_up, .icon-thumb_down
+              margin-right: 4px
+              line-height: 16px
+              font-size: $fontsize-small
+            .icon-thumb_up
+              color: $color-blue
+            .icon-thumb_down
+              color: $color-light-grey
 
 </style>
 
